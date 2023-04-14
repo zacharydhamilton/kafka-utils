181bb4f256ff776f05a8cb904dca934ecf1fbbc0
Zachary Hamilton <zdhamilton@confluent.io>
2023-04-14 13:57:45 -0400

    Added additional methods

--- src/main/java/com/github/zacharydhamilton/kafka/common/Utils.java
+++ src/main/java/com/github/zacharydhamilton/kafka/common/Utils.java
@@ -8,19 +8,24 @@ import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.Properties;
 
+import org.apache.kafka.clients.CommonClientConfigs;
 import org.apache.kafka.common.config.ConfigException;
+import org.apache.kafka.common.config.SaslConfigs;
+
+import io.confluent.kafka.serializers.AbstractKafkaSchemaSerDeConfig;
 
 public class Utils {
     /**
-     * Check for the necessary configurations to initialize the Ksql client. If any are missing, fails. 
+     * Check for the necessary configurations to initialize. If any are missing, fails. 
      * 
+     * @param configuredProps - A Properties object containing the 
+     * @param requiredProps - An ArrayList of Strings representing the required properties to check for. 
      * @throws ConfigException
      */
-    public static void preInitChecks(ArrayList<String> assertedProps) throws ConfigException {
-        ArrayList<String> requiredProps = assertedProps;
+    public static void preInitChecks(Properties configuredProps, ArrayList<String> requiredProps) throws ConfigException {
         ArrayList<String> missingProps = new ArrayList<String>();
         for (String prop : requiredProps) {
-            if (System.getenv(prop).equals(null)) {
+            if (configuredProps.get(prop).equals(null)) {
                 missingProps.add(prop);
             }
         }
@@ -43,5 +48,43 @@ public class Utils {
         try (InputStream inputStream = new FileInputStream(file)) {
             props.load(inputStream);
         }
+    }
+
+    /**
+     * 
+     * @param props - An existing Properties object to add the properties to.
+     * @param assertedProps - An ArrayList of Strings representing the required properties to add.
+     * @throws ConfigException
+     */
+    public static void addPropsFromEnv(Properties props, ArrayList<String> requiredProps) throws ConfigException {
+        String kafkaKey = "";
+        String kafkaSecret = "";
+        String srKey = "";
+        String srSecret = "";
+        for (String prop : requiredProps) {
+            switch (prop) {
+                case "BOOTSTRAP_SERVERS":
+                    props.put(CommonClientConfigs.BOOTSTRAP_SERVERS_CONFIG, System.getenv(prop));
+                    break;
+                case "KAFKA_KEY": 
+                    kafkaKey = System.getenv(prop);
+                    break;
+                case "KAFKA_SECRET": 
+                    kafkaSecret = System.getenv(prop);
+                case "SCHEMA_REGISTRY_URL":
+                    props.put(AbstractKafkaSchemaSerDeConfig.SCHEMA_REGISTRY_URL_CONFIG, System.getenv(prop));
+                    break;
+                case "SCHEMA_REGISTRY_KEY":
+                    srKey = System.getenv(prop);
+                    break;
+                case "SCHEMA_REGISTRY_SECRET":
+                    srSecret = System.getenv(prop);
+                default: 
+                    throw new ConfigException("Unsupported or unknown config: " + prop);
+            }
+        }
+        props.put(SaslConfigs.SASL_JAAS_CONFIG, "org.apache.kafka.common.security.plain.PlainLoginModule   required username='"+kafkaKey+"'   password='"+kafkaSecret+"';");
+        props.put(AbstractKafkaSchemaSerDeConfig.BASIC_AUTH_CREDENTIALS_SOURCE, "USER_INFO");
+        props.put(AbstractKafkaSchemaSerDeConfig.USER_INFO_CONFIG, srKey+":"+srSecret);
     }
 }
